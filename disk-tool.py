4b20127412a3936c78e483345ee7196a214c6719
Marty @ FW FDTK <marty@freedom-tek.com>
2023-04-24 10:56:17 +0800

    改成disk name 方式呈現

--- disk-tool.py
+++ disk-tool.py
@@ -4,49 +4,63 @@
 #
 import picotui.screen as scn
 import picotui.widgets as wgs
+import re
 
 # from picotui.defs import C_B_WHITE, C_WHITE, C_B_BLUE, C_BLACK
 import picotui.defs as defs
+
+
 def linux_cmd(command):
     import subprocess
+
     output = subprocess.check_output(command, shell=True)
     return output.decode()
+
+
 def linux_cmd_json_dict(command):
     import json
+
     output = linux_cmd(command)
     return json.loads(output)
+
+
+def linux_all_disk_list():
+    output = linux_cmd(hdd_test_cmd_disk_device_list).split("\n")
+    print(output)
+    return output
+
+
 # print(linux_cmd("sudo fdisk -l"))
 # print(linux_cmd_json_dict("sudo lshw -c disk -json"))
 hdd_test_cmd_smartctl = "sudo smartctl -x /dev/cdrom -j"
 hdd_test_cmd_partition_info = "sudo sgdisk -p /dev/sda"
-hdd_test_cmd_clean_disk_partition_head = "sudo dd if=/dev/zero of=/dev/sdc status=progress bs=512 count=34"
+hdd_test_cmd_clean_disk_partition_head = (
+    "sudo dd if=/dev/zero of=/dev/sdc status=progress bs=512 count=34"
+)
 hdd_test_cmd_clean_disk_partition_tail = "sudo dd if=/dev/zero of=/dev/sdc bs=512 count=34 seek=$((`sudo /sbin/blockdev --getsz /dev/sdb` - 34))"
-hdd_test_cmd_read_whole_disk = "sudo dd if=/dev/sdc of=/dev/null status=progress bs=256M conv=sync,noerror"
-hdd_test_cmd_write_whole_disk = "sudo dd if=/dev/zero of=/dev/sdc status=progress bs=256M conv=sync,noerror"
-hdd_test_cmd_disk_device_list = "sudo lshw -c disk -short -quiet |grep disk|cut -c 22-33,49-"
+hdd_test_cmd_read_whole_disk = (
+    "sudo dd if=/dev/sdc of=/dev/null status=progress bs=256M conv=sync,noerror"
+)
+hdd_test_cmd_write_whole_disk = (
+    "sudo dd if=/dev/zero of=/dev/sdc status=progress bs=256M conv=sync,noerror"
+)
+hdd_test_cmd_disk_device_list = "sudo lshw -c disk -short -quiet |grep disk"
 if __name__ == "__main__":
     import os
     import subprocess
     import json
+
     s = scn.Screen()
 
-    dev_disk = "/dev/"
-    disk_list = []
-    for file in os.listdir(dev_disk):
-        if file.startswith("sd"):
-            disk_list.append(file)
+    disks_dict = {}
+    for i in linux_all_disk_list():
+        single_disk_info = re.split(r"\s{2,}", i)
+        if i:
+            disks_dict[single_disk_info[3]] = single_disk_info[1]
 
-    choices = disk_list
-    disks_info_dict = linux_cmd_json_dict("sudo lshw -c disk -json -quiet")
-    disks_logicalname_dict = {}
-    for i in disks_info_dict:
-        print(i["product"],i["logicalname"],i["description"])
-        if isinstance(i["logicalname"], list):
-            disks_logicalname_dict[i["logicalname"][0]] = {"product":i["product"],"description":i["description"]}
-        else:
-            disks_logicalname_dict[i["logicalname"]] = {"product":i["product"],"description":i["description"]}
-
-
+    print(disks_dict)
+    # choices = disks_dict.items()
+    choices = list(disks_dict.keys())
     try:
         s.init_tty()
         s.enable_mouse()
@@ -57,9 +71,10 @@ if __name__ == "__main__":
 
         # DropDown and ListBox widgets
         d.add(1, 1, "選擇硬碟:")
-        w_dropdown_target_disk = wgs.WDropDown(15, ["All"] + list(disks_logicalname_dict.keys()), dropdown_h=6)
+        w_dropdown_target_disk = wgs.WDropDown(
+            15, ["All"] + list(disks_dict.items()), dropdown_h=6
+        )
         d.add(11, 1, w_dropdown_target_disk)
-
 
         d.add(1, 3, "List:")
         w_listbox = wgs.WListBox(24, 6, choices)
@@ -113,5 +128,3 @@ if __name__ == "__main__":
     # os.system("sudo dd of=/dev/null if=/dev/sda3 status=progress")
     # os.system("sudo smartctl -a /dev/sda")
     # os.system("sudo fdisk -l")
-
-
